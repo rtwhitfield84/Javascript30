2c5b03ce22df26a57d48e2b74ef9b3d6ab7ef166
Rico <richardthomaswhitfield@gmail.com>
2017-04-24 18:01:58 -0500

    JS drum kit

---
+++ 01 - JavaScript Drum Kit/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+window.addEventListener('keydown', (e) => {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+	if(!audio) return; //stop function from running
+	audio.currentTime = 0;
+	audio.play();
+	key.classList.add('playing');
+});
+
+function removeTransition (e) {
+	if (e.propertyName !== 'transform') return;
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
