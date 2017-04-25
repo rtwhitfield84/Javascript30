d1ec567dae800d3467318793859528c6e027d983
Rico <richardthomaswhitfield@gmail.com>
2017-04-25 15:41:07 -0500

    finished updating vars via js

--- 03 - CSS Variables/main.js
+++ 03 - CSS Variables/main.js
@@ -1,2 +1,13 @@
 "use strict";
 
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+	const suffix = this.dataset.sizing || '';
+	document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
+	console.log(this.value);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
